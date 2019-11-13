7536cc8b6f3f3b0856ea004073e4896f417ae6b7
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2019-11-13 19:51:51 +0100

    Abstraction

--- editor.js
+++ editor.js
@@ -1,5 +1,18 @@
-class Editor{
-    constructor(){
+import {Document} from "pmeditor-core"
+
+export class Editor {
+    constructor(document = null) {
+        if (!document)
+            document = new Document();
+        this.document = document;
+        this.html = document.createElement('div');
+        this.input = document.createElement('input');
+        this.html.append(this.input);
+        this.html.onkeypress = this.keyPress.bind(this);
+    }
+
+    keyPress(e) {
+        console.log(e);
 
     }
 }
