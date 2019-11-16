ffec687886e3d8e8de24606767139b384c29adb5
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2019-11-16 12:12:37 +0100

    .

--- editor.js
+++ editor.js
@@ -1,4 +1,4 @@
-import {Document} from "pmeditor-core"
+import {Document, BlockAbstract, TextNode} from "pmeditor-core"
 
 export class Editor {
     constructor(doc = null) {
@@ -13,6 +13,32 @@ export class Editor {
 
     keyPress(e) {
         console.log(e);
-this.doc.appendText(e.key);
+        this.doc.appendText(e.key);
+    }
+
+    rerender() {
+        Array.from(this.html.childNodes).filter(x => x != this.input).forEach(x => x.remove());
+        for (let element of this.doc.content) {
+            this.html.append(this.render(element))
+        }
+    }
+
+    render(element) {
+        if (element instanceof BlockAbstract) {
+            let html = document.createElement('div');
+            let contentArray = this.renderBlockContent(element.content);
+            contentArray.forEach(x => html.append(x));
+            return html;
+        }
+    }
+
+    renderBlockContent(contentArray) {
+        let ret = [];
+        for (let element of contentArray) {
+            if(element instanceof TextNode){
+                ret.push(document.createTextNode(element.content))
+            }
+        }
+        return ret;
     }
 }
