9085915bb6b765ef258c6961c24e8424614c77f3
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2019-11-23 11:43:27 +0100

    merge fixes

--- editor.js
+++ editor.js
@@ -35,6 +35,61 @@ export class Editor {
 
     keyPress(e) {
         console.log(e);
-this.doc.appendText(e.key);
+        this.cursor = this.doc.addText(e.key, this.cursor);
+        this.render();
+    }
+
+    rerender() {
+        this.render();
+    }
+
+    render() {
+        let next = this.doc.content.map(x => this.renderElement(x));
+        this.replaceChildrens(this.docHtml, next);
+    }
+
+    replaceChildrens(element, next) {
+        let current = Array.from(element.childNodes);
+        for (let x of next) {
+            element.append(x)
+        }
+        for (let x of current) {
+            if (!next.includes(x)) {
+                x.remove();
+            }
+        }
+    }
+
+    renderElement(element) {
+        if (element instanceof BlockAbstract) {
+            let html = document.createElement('div');
+            let contentArray = this.renderBlockContent(element.content);
+            contentArray.forEach(x => html.append(x));
+            element.contentChanged(() => {
+                let contentArray = this.renderBlockContent(element.content);
+                this.replaceChildrens(html, contentArray);
+            });
+            return html;
+        }
+    }
+
+    renderBlockContent(contentArray) {
+        let ret = [];
+        for (let element of contentArray) {
+            if (element instanceof TextNode) {
+                const cursorNode = this.cursor[this.cursor.length - 2];
+                if (cursorNode == element) {
+                    const cursorOffset = this.cursor[this.cursor.length - 1] || 0;
+                    ret.push(document.createTextNode(element.content.substr(0, cursorOffset + 1)))
+                    ret.push(this.cursorHtml);
+                    ret.push(document.createTextNode(element.content.substr(cursorOffset + 1)))
+
+                } else {
+                    ret.push(document.createTextNode(element.content))
+
+                }
+            }
+        }
+        return ret;
     }
 }
